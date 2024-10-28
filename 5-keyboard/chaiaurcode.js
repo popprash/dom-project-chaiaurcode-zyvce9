c2d4588edbee8b25126af1a36f09fb08d249954a
popprash <162835525+popprash@users.noreply.github.com>
2024-10-28 12:04:32 +0530

    Update chaiaurcode.js

--- 5-keyboard/chaiaurcode.js
+++ 5-keyboard/chaiaurcode.js
@@ -1,1 +1,20 @@
 console.log('Project 5');
+const insert = document.querySelector('#insert')
+
+window.addEventListener('keydown', (e) => {
+  insert.innerHTML= `
+  <table>
+  <tr>
+    <th>KEY</th>
+    <th>KEycCode</th>
+    <th>Code</th>
+  </tr>
+  <tr>
+    <td>${e.key}</td>
+    <td>${e.keyCode}</td>
+    <td>${e.code}</td>
+  </tr>
+  <tr>
+    
+</table>`
+})console.log('Project 5');
